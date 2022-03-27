12137fd7430f29d7ace828ba873c50978c1ca24b
Paul Moros <pmoros@outlook.com>
2022-03-27 18:17:47 -0500

    test(member): add initialization

--- tests/unit/test_member.py
+++ tests/unit/test_member.py
@@ -5,3 +5,6 @@ from python_learning.member import Member
 class TestMember(TestCase):
     def setUp(self):
         self.member = Member()
+
+    def test_initialization(self):
+        self.assertEqual(isinstance(self.member, Member), True)
