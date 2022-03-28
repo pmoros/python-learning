4b59cc3132adfab9f6d01340305bc78ab981f74a
Paul Moros <pmoros@outlook.com>
2022-03-27 20:29:41 -0500

    test(member): add set mother

--- python_learning/member.py
+++ python_learning/member.py
@@ -1,3 +1,30 @@
+from enum import Enum
+
+
+import enum
+
+
+class Gender(enum.Enum):
+    male = "Male"
+    female = "Female"
+
+
 class Member:
-    def __init__(self):
-        pass
+    def __init__(self, id, name, gender):
+        self.id = id
+        self.name = name
+        self.gender = gender
+        self.mother = None
+        self.father = None
+        self.spouse = None
+        self.children = []
+
+    def set_mother(self, mother):
+        if not isinstance(mother, Member):
+            raise TypeError("Invalid value for mother")
+        elif mother.gender != Gender.female:
+            raise ValueError(
+                "Invalid gender value for mother" "Mother should be female"
+            )
+        else:
+            self.mother = mother
--- tests/unit/test_member.py
+++ tests/unit/test_member.py
@@ -1,10 +1,37 @@
+from multiprocessing.sharedctypes import Value
 from unittest import TestCase
-from python_learning.member import Member
+from python_learning.member import Member, Gender
 
 
 class TestMember(TestCase):
     def setUp(self):
-        self.member = Member()
+        self.member = Member(1, "Zim", Gender.male)
 
     def test_initialization(self):
+        # Check instance
         self.assertEqual(isinstance(self.member, Member), True)
+
+        # Check properties
+        self.assertEqual(self.member.id, 1)
+        self.assertEqual(self.member.name, "Zim")
+        self.assertEqual(self.member.gender, Gender.male)
+        self.assertEqual(self.member.mother, None)
+        self.assertEqual(self.member.father, None)
+        self.assertEqual(self.member.spouse, None)
+        self.assertEqual(self.member.children, [])
+
+        # Edge case for gender
+
+    def test_set_mother(self):
+        mother_demo_a = "mother_demo_a"  # Wrong type
+        mother_demo_b = Member(2, "MotherDemoB", Gender.male)  # Invalid sex
+        mother_demo_c = Member(3, "Mom", Gender.female)
+
+        # Error case
+        self.assertRaises(TypeError, self.member.set_mother, mother_demo_a)
+        self.assertRaises(ValueError, self.member.set_mother, mother_demo_b)
+
+        # Success case
+        self.member.set_mother(mother_demo_c)
+        self.assertEqual(self.member.mother.name, "Mom")
+        self.assertEqual(self.member.mother.gender, Gender.female)
