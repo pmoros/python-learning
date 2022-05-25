46e79e20d2e8b78644989d127608c3791f74e725
pmoros <pmoros@outlook.com>
2022-05-25 14:32:18 -0500

    feat: Calculator

---
+++ app/math_operations.py
@@ -0,0 +1,15 @@
+"""
+    Defines basic math operations.
+"""
+
+
+class Calculator:
+    """Arithmetic calculator."""
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def divide(a, b):
+        pass
--- tests/unit/test_math_operations.py
+++ tests/unit/test_math_operations.py
@@ -4,8 +4,8 @@ from app.math_operations import Calculator
 
 class TestCalculator(unittest.TestCase):
     def test_should_add_two_numbers(self):
-        left_number = 1
-        right_number = 4
+        a = 1
+        b = 4
 
-        result = Calculator.add(left_number, right_number)
+        result = Calculator.add(a, b)
         self.assertEqual(result, 5)
