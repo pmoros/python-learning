78d6c1ca756a625a710fbf5d95913c3c9b7d6d30
pmoros <pmoros@outlook.com>
2022-05-28 16:52:19 -0500

    refactor(test): side effect calls function (fake)

--- tests/unit/test_calendar_service.py
+++ tests/unit/test_calendar_service.py
@@ -31,3 +31,26 @@ class TestCalendarService(unittest.TestCase):
     def test_should_get_holidays_raise_timeout(self, mock_requests_get):
         with self.assertRaises(TimeoutError):
             calendar_service.get_holidays()
+
+    def log_request(self, url):
+        # Log a fake request for test output purposes
+        print(f"Making a request to {url}.")
+        print("Request received!")
+
+        # Create a new Mock to imitate a Response
+        response_mock = mock.Mock()
+        response_mock.status_code = 200
+        response_mock.json.return_value = {
+            "12/25": "Christmas",
+            "7/4": "Independence Day",
+        }
+        return response_mock
+
+    @mock.patch(
+        "app.services.calendar_service.requests.get",
+        autospec=True,
+    )
+    def test_get_holidays_logging(self, mock_requests_get):
+        # Test a successful, logged request
+        mock_requests_get.side_effect = self.log_request
+        assert calendar_service.get_holidays()["12/25"] == "Christmas"
