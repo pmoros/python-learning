da27a7fe804872561c0c3be7055b633e9f44c02d
pmoros <pmoros@outlook.com>
2022-05-27 20:31:35 -0500

    feat(shell): UploadService upload_complete method

--- app/shell.py
+++ app/shell.py
@@ -8,3 +8,14 @@ class RemovalService:
     def rm(self, file):
         if os.path.isfile(file):
             os.remove(file)
+
+
+class UploadService:
+    """A service for uploading files to filesystem."""
+
+    def __init__(self, removal_service):
+        self.removal_service = removal_service
+
+    def upload_complete(self, file):
+        # TODO: upload file to filesystem
+        self.removal_service.rm(file)
